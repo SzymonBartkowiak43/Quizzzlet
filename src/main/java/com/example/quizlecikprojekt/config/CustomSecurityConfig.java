2b367125042b70d9026d18dfd30cd7785b1a5c9c
Szymon Bartkowiak <szymon.b4310@gmail.com>
2024-07-13 12:47:56 +0200

    Zrobienie wlasnego formularza logowania

--- src/main/java/com/example/quizlecikprojekt/config/CustomSecurityConfig.java
+++ src/main/java/com/example/quizlecikprojekt/config/CustomSecurityConfig.java
@@ -3,9 +3,9 @@ package com.example.quizlecikprojekt.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class CustomSecurityConfig {
@@ -16,7 +16,17 @@ public class CustomSecurityConfig {
         http.authorizeHttpRequests(requests -> requests
                 .requestMatchers("/").permitAll()
                 .requestMatchers("/log").hasAnyRole(USER_ROLE, ADMIN_ROLE))
-        .formLogin(withDefaults());
+        .formLogin(login -> login
+                .loginPage("/login")
+                .permitAll()
+        );
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers(
+                "/styles/**"
+        );
     }
 }
--- src/main/resources/db/testdata/0001_users_and_roles.sql
+++ src/main/resources/db/testdata/0001_users_and_roles.sql
@@ -1,20 +1,17 @@
 insert into
     users (email, password)
 values
-    ('admin@example.com', '{noop}adminpass'),   -- 1
-    ('user@example.com', '{noop}userpass'),     -- 2
-    ('editor@example.com', '{noop}editorpass'); -- 3
+    ('admin@wp.com', '{noop}admin'),
+    ('user@wp.com', '{noop}user');
 
 insert into
     user_role (name, description)
 values
-    ('ADMIN', 'pełne uprawnienia'),   -- 1
-    ('USER', 'podstawowe uprawnienia, możliwość oddawania głosów'),   -- 2
-    ('EDITOR', 'podstawowe uprawnienia + możliwość zarządzania treściami');   -- 3
+    ('ADMIN', 'pełne uprawnienia'),
+    ('USER', 'podstawowe uprawnienia, możliwość oddawania głosów');
 
 insert into
     user_roles (user_id, role_id)
 values
     (1, 1),
-    (2, 2),
-    (3, 3);
+    (2, 2);
