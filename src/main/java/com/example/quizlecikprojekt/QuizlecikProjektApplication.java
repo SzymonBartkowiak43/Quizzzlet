47bfd5f68a904a0a7a7d7d68e35f1d44b28e6e1d
Szymon Bartkowiak <szymon.b4310@gmail.com>
2024-08-12 12:16:50 +0200

    logger

--- src/main/java/com/example/quizlecikprojekt/QuizlecikProjektApplication.java
+++ src/main/java/com/example/quizlecikprojekt/QuizlecikProjektApplication.java
@@ -1,12 +1,16 @@
 package com.example.quizlecikprojekt;
 
+import com.example.quizlecikprojekt.web.RegistrationController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QuizlecikProjektApplication {
-
+    private final static Logger LOGGER = LoggerFactory.getLogger(RegistrationController.class);
     public static void main(String[] args) {
+        LOGGER.info("Starting QuizlecikProjektApplication");
         SpringApplication.run(QuizlecikProjektApplication.class, args);
     }
 
